e2fad84c7846437057e722ac615521db7caf63ab
Johann Tuffe <tafia973@gmail.com>
2015-09-03 12:14:38 +0800

    add FloatExt trait

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,19 @@
 extern crate num;
 
 mod time;
+
+use num::traits::Float;
+use std::num::{Zero, One};
+use std::ops::{Add, Sub, Mul};
+
+/// General purpose float trait for finance
+pub trait FloatExt : Float + Zero + One + Add<Self> + Sub<Self> + Mul<Self> {
+    /// Set Self = Self / 2
+    fn half(&mut self);
+}
+
+impl FloatExt for f64 {
+    fn half(&mut self) {
+        *self *= 0.5;
+    }
+}
--- src/time.rs
+++ src/time.rs
@@ -1,54 +1,36 @@
-use num::traits::Float;
 use std::num::{Zero, One};
-use std::ops::{Add, Sub, Mul};
 use std::mem;
 use std::fmt::Debug;
-
-pub trait Half {
-    fn half(&mut self);
-}
-
-impl Half for f64 {
-    fn half(&mut self) {
-        *self *= 0.5;
-    }
-}
-
-impl Half for f32 {
-    fn half(&mut self) {
-        *self *= 0.5;
-    }
-}
+use std::ops::Range;
+use super::FloatExt;
 
 /// Calculates the present value of future cash flows with discrete compounding
-pub fn cash_flow_pv_discrete<F>(times: &[F], amounts: &[F], r: &F) -> F
-        where F: Float + Add<F> + Zero + One {
+pub fn cash_flow_pv_discrete<F: FloatExt>(times: &[F], amounts: &[F], r: &F) -> F {
     let acc = <F as One>::one() + *r;
     amounts.iter().zip(times.iter())
     .map(|(&amount, &t)| amount / acc.powf(t)).sum()
 }
 
 /// Calculates the present value of future cash flows with continuous compounded interest
-pub fn cash_flow_pv<F>(times: &[F], amounts: &[F], r: &F) -> F
-        where F: Float + Add<F> + Zero {
+pub fn cash_flow_pv<F: FloatExt>(times: &[F], amounts: &[F], r: &F) -> F {
     amounts.iter().zip(times.iter())
     .map(|(&amount, &t)| amount * (-(*r * t)).exp()).sum()
 }
 
 /// Calculates the internal rate of return
 /// Initial bucket is set to F::zero()..F::one()
-pub fn cash_flow_irr<F>(times: &[F], amounts: &[F], accuracy: &F, max_iteration: usize) -> Option<F>
-        where F: Float + Add<F> + Zero + One + Sub<F> + Mul<F> + Half {
+pub fn cash_flow_irr<F: FloatExt>(times: &[F], amounts: &[F],
+    accuracy: &F, max_iteration: usize, bucket: &Range<F>) -> Option<F> {
 
-    let (zero, one) = (<F as Zero>::zero(), <F as One>::one());
-    let (mut x1, mut x2) = (zero, one);
+    let (mut x1, mut x2) = (bucket.start, bucket.end);
     let (mut f1, mut f2) = (cash_flow_pv(times, amounts, &x1), cash_flow_pv(times, amounts, &x2));
 
+    let zero = <F as Zero>::zero();
     if f1 * f2 > zero {
         return None;
     }
 
-    // order x1/x2 f1/f2
+    // sort x1/x2 f1/f2
     if f1 > zero {
         mem::swap(&mut x1, &mut x2);
         mem::swap(&mut f1, &mut f2)
@@ -83,6 +65,6 @@ fn test_cash_flow_pv() {
 
 #[test]
 fn test_cash_flow_irr() {
-    let a = cash_flow_irr(&[1.0, 2.0, 3.0], &[-2f64, 1.0, 1.0], &1.0e-5, 50);
+    let a = cash_flow_irr(&[1.0, 2.0, 3.0], &[-2f64, 1.0, 1.0], &1.0e-5, 50, &(0.0f64..1.0));
     assert_eq!(a, Some(0.9999923706054688));
 }
