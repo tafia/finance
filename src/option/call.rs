ef4c7942ece644bd20e08303f85c7e6041d69466
Johann Tuffe <tafia973@gmail.com>
2015-09-15 19:54:22 +0800

    rename call

--- src/option/call.rs
+++ src/option/call.rs
@@ -1,13 +1,6 @@
 use math::{standard_normal_cdf, standard_normal_pdf};
 use ::{MAX_ITERATIONS, HIGH_VALUE, ACCURACY};
-
-pub struct Greeks {
-    pub delta: f64,
-    pub gamma: f64,
-    pub theta: f64,
-    pub vega: f64,
-    pub rho: f64,
-}
+use option::Greeks;
 
 /// European option price
 /// Can be exerced only at a given date
@@ -16,7 +9,7 @@ pub struct Greeks {
 /// r: interest rate (per period)
 /// sigma: standard deviation of underlying asset
 /// t: time to maturity
-pub fn call_price(s: f64, k: f64, r: f64, sigma: f64, t: f64, greeks: &mut Option<Greeks>) -> f64 {
+pub fn price(s: f64, k: f64, r: f64, sigma: f64, t: f64, greeks: &mut Option<Greeks>) -> f64 {
     let sqrt_t = t.sqrt();
     let sig_sqrt_t = sigma * sqrt_t;
     let d1 = ((s / k).ln() + r * t) / sig_sqrt_t + sig_sqrt_t / 2.0;
@@ -37,30 +30,30 @@ pub fn call_price(s: f64, k: f64, r: f64, sigma: f64, t: f64, greeks: &mut Optio
     s * cdf1 - k * (-r * t).exp() * cdf2
 }
 
-pub fn implied_volatility_call_bissection(s: f64, k: f64, r: f64, t: f64, option_price: f64) ->
+pub fn implied_volatility_bissection(s: f64, k: f64, r: f64, t: f64, option_price: f64) ->
     Result<f64, &'static str>
 {
     let mut sigma_low = 1.0e-5; // check for arbitrage violation
     let mut no_greek = None;
-    let mut price = call_price(s, k, r, sigma_low, t, &mut no_greek);
+    let mut price_ = price(s, k, r, sigma_low, t, &mut no_greek);
 
-    if price > option_price {
+    if price_ > option_price {
         return Ok(0.0);
     }
 
     let mut sigma_high = 0.3f64;
-    price = call_price(s, k, r, sigma_high, t, &mut no_greek);
-    while price < option_price {
+    price_ = price(s, k, r, sigma_high, t, &mut no_greek);
+    while price_ < option_price {
         sigma_high *= 2.0;
-        price = call_price(s, k, r, sigma_high, t, &mut no_greek);
+        price_ = price(s, k, r, sigma_high, t, &mut no_greek);
         if sigma_high > HIGH_VALUE {
             return Err("Something wrong");
         }
     }
     for _ in 0..MAX_ITERATIONS {
         let sigma = (sigma_low + sigma_high) * 0.5;
-        price = call_price(s, k, r, sigma, t, &mut no_greek);
-        let diff = price - option_price;
+        price_ = price(s, k, r, sigma, t, &mut no_greek);
+        let diff = price_ - option_price;
         if diff.abs() < ACCURACY {
             return Ok(sigma);
         }
@@ -73,22 +66,22 @@ pub fn implied_volatility_call_bissection(s: f64, k: f64, r: f64, t: f64, option
     Err("Cannot find implied volatility")
 }
 
-pub fn implied_volatility_call_newton(s: f64, k: f64, r: f64, t: f64, option_price: f64) ->
+pub fn implied_volatility_newton(s: f64, k: f64, r: f64, t: f64, option_price: f64) ->
     Result<f64, &'static str>
 {
     let mut sigma_low = 1.0e-5; // check for arbitrage violation
     let mut no_greek = None;
-    let mut price = call_price(s, k, r, sigma_low, t, &mut no_greek);
+    let mut price_ = price(s, k, r, sigma_low, t, &mut no_greek);
 
-    if price > option_price {
+    if price_ > option_price {
         return Ok(0.0);
     }
 
     let sqrt_t = t.sqrt();
     let mut sigma = (option_price / s) / (0.398 * sqrt_t);
     for _ in 0..MAX_ITERATIONS {
-        price = call_price(s, k, r, sigma, t, &mut no_greek);
-        let diff = option_price - price;
+        price_ = price(s, k, r, sigma, t, &mut no_greek);
+        let diff = option_price - price_;
         if diff.abs() < ACCURACY {
             return Ok(sigma);
         }
--- src/option/mod.rs
+++ src/option/mod.rs
@@ -1,2 +1,10 @@
 mod binomial;
-mod black_scholes;
+mod call;
+
+pub struct Greeks {
+    pub delta: f64,
+    pub gamma: f64,
+    pub theta: f64,
+    pub vega: f64,
+    pub rho: f64,
+}
